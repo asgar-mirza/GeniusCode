2a2c178eb08ba34bf40361d85dbf6513437b9305
Asgar Mirza <asgarmirza5@gmail.com>
2022-12-03 07:37:47 +0530

    :dead

--- pages/index.js
+++ pages/index.js
@@ -12,15 +12,19 @@ const callGenerateEndpoint = async () => {
   setIsGenerating(true);
   
   console.log("Calling OpenAI...")
+
+  
+
   const response = await fetch('/api/generate', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ userInput }),
-  });
-  
-  response.catch(err => alert(err))
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
 
   const data = await response.json()
   const { output } = data;
